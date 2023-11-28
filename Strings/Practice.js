06f292297298757fb0783bba24bdce280ec175c6
Veervikram singh <veervikramsingh132@gmail.com>
2023-11-28 21:30:11 +0530

    practice

--- Strings/Practice.js
+++ Strings/Practice.js
@@ -1,16 +1,16 @@
-// function Practice(array) {
-//   if (array.length === 0) return 0;
-//   let i = 0;
+function Practice(array) {
+  if (array.length === 0) return 0;
+  let i = 0;
 
-//   const arr = [];
-//   for (let j = 1; j < array.lenth; j++) {
-//     if (array[i] !== array[j]) {
-//       i++;
-//       array[i] = array[j];
-//       arr.push(i);
-//     }
-//   }
-//   console.log(arr);
-// }
+  const arr = [];
+  for (let j = 1; j < array.lenth; j++) {
+    if (array[i] !== array[j]) {
+      i++;
+      array[i] = array[j];
+      arr.push(i);
+    }
+  }
+  console.log(arr);
+}
 
-// Practice([1, 2, 2, 2, 3, 3, 4, 5, 6, 6]);
+Practice([1, 2, 2, 2, 3, 3, 4, 5, 6, 6]);
